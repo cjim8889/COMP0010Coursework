66afbd0c1f5fca42d1cd3d81ba591fabea6d5287
Wuhao Chen <c.jim8889@outlook.com>
2019-10-19 19:18:43 +0100

    remove unused code

--- src/main/java/uk/ac/ucl/jsh/app/Ls.java
+++ src/main/java/uk/ac/ucl/jsh/app/Ls.java
@@ -4,12 +4,9 @@ package uk.ac.ucl.jsh.app;
 import uk.ac.ucl.jsh.Core;
 
 import java.io.File;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 
 public class Ls extends AbstractApp implements App{
-    private File currentDir;
     private String argument = "";
 
 
@@ -17,21 +14,8 @@ public class Ls extends AbstractApp implements App{
         super(jshCore);
     }
 
-    public boolean checkArg(ArrayList<String> arg){
-        boolean bool;
-        if (arg.isEmpty()){
-            currentDir = new File(String.valueOf(jshCore.getCurrentDirectory()));
-            bool = true;
-        }else if (arg.size()==1){
-            currentDir = new File(arg.get(0));
-            bool = true;
-        }else {
-            throw new RuntimeException("Incorrect Number Of Arguments!");
-        }
-        return bool;
-    }
-
     public void run() {
+        File currentDir;
         if (argument.length() > 0) {
             currentDir = new File(Paths.get(argument).toString());
         } else {
