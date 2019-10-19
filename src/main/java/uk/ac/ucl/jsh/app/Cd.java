036d0ae30a99268a91537fdfeafe8f073e191fb5
Wuhao Chen <c.jim8889@outlook.com>
2019-10-19 16:46:38 +0100

    cd now support ../ syntax

--- src/main/java/uk/ac/ucl/jsh/app/Cd.java
+++ src/main/java/uk/ac/ucl/jsh/app/Cd.java
@@ -3,6 +3,7 @@ package uk.ac.ucl.jsh.app;
 import uk.ac.ucl.jsh.Core;
 import uk.ac.ucl.jsh.Descriptor;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -23,7 +24,6 @@ public class Cd extends AbstractApp implements App {
 
     private void initialise() {
         systemType = jshCore.getSystemType();
-
     }
 
     public void run() throws RuntimeException {
@@ -53,7 +53,6 @@ public class Cd extends AbstractApp implements App {
         String windowsStartRegex = "([A-Za-z]:\\\\)";
         Pattern regex = Pattern.compile(windowsStartRegex);
         Matcher regexMatcher = regex.matcher(argument);
-
         if (regexMatcher.find()) {
             destinationPath = Paths.get(argument);
         } else {
@@ -66,6 +65,8 @@ public class Cd extends AbstractApp implements App {
             destinationPath = Paths.get(argument);
         } else if (argument.startsWith("./")) {
             destinationPath = Paths.get(jshCore.getCurrentDirectory().toAbsolutePath().toString() + jshCore.getPathSeparator() + argument.substring(1));
+        } else if (argument.startsWith("../")) {
+            destinationPath = Paths.get(jshCore.getCurrentDirectory().getParent().toAbsolutePath().toString() + jshCore.getPathSeparator() + argument.substring(2));
         } else {
             destinationPath = Paths.get(jshCore.getCurrentDirectory().toAbsolutePath().toString() + jshCore.getPathSeparator() + argument);
         }
